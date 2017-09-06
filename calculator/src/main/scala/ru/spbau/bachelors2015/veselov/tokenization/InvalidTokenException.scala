7fcd23151accf666b1b3c086dcdef8444e0e4a1d
Ivan Veselov <veselov.iva@gmail.com>
2017-09-06 23:11:08 +0300

    Add scaladocs to exceptions.

--- calculator/src/main/scala/ru/spbau/bachelors2015/veselov/tokenization/InvalidTokenException.scala
+++ calculator/src/main/scala/ru/spbau/bachelors2015/veselov/tokenization/InvalidTokenException.scala
@@ -1,3 +1,6 @@
 package ru.spbau.bachelors2015.veselov.tokenization
 
+/**
+  * Thrown from Tokenizer when no valid token can be matched.
+  */
 class InvalidTokenException extends Exception
--- calculator/src/main/scala/ru/spbau/bachelors2015/veselov/tokenization/NoTokensLeftException.scala
+++ calculator/src/main/scala/ru/spbau/bachelors2015/veselov/tokenization/NoTokensLeftException.scala
@@ -1,3 +1,6 @@
 package ru.spbau.bachelors2015.veselov.tokenization
 
+/**
+  * Thrown from Tokenizer when no tokens left but next token is requested.
+  */
 class NoTokensLeftException extends Exception
