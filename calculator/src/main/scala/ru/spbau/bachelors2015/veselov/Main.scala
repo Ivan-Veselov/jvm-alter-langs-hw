a52be42f29d0f4377fc94a92134cb322a43ec95e
Ivan Veselov <veselov.iva@gmail.com>
2017-09-07 23:47:38 +0300

    Add console interface.

--- calculator/src/main/scala/ru/spbau/bachelors2015/veselov/Main.scala
+++ calculator/src/main/scala/ru/spbau/bachelors2015/veselov/Main.scala
@@ -1,6 +1,21 @@
 package ru.spbau.bachelors2015.veselov
 
+import ru.spbau.bachelors2015.veselov.evaluation.{EmptyExpressionException, ExpressionEvaluator, InvalidExpressionException}
+import ru.spbau.bachelors2015.veselov.tokenization.InvalidTokenException
+
+import scala.io.StdIn
+
 object Main {
   def main(args: Array[String]): Unit = {
+    Console.print("Enter arithmetical expression:\n")
+
+    try {
+      Console.print(ExpressionEvaluator.eval(StdIn.readLine()))
+    } catch {
+      case _: EmptyExpressionException =>
+        Console.print("Empty expression is not allowed")
+      case _ : InvalidTokenException | _ : InvalidExpressionException =>
+        Console.print("Invalid expression entered")
+    }
   }
 }
