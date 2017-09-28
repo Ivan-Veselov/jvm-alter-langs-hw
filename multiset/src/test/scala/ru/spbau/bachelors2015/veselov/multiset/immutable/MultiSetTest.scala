ddee0ac02b3267a46d73e940ec304c84af1b8b39
Ivan Veselov <veselov.iva@gmail.com>
2017-09-28 23:02:17 +0300

    Add tests for non empty set.

--- multiset/src/test/scala/ru/spbau/bachelors2015/veselov/multiset/immutable/MultiSetTest.scala
+++ multiset/src/test/scala/ru/spbau/bachelors2015/veselov/multiset/immutable/MultiSetTest.scala
@@ -3,7 +3,6 @@ package ru.spbau.bachelors2015.veselov.multiset.immutable
 import org.scalatest.{FlatSpec, Matchers}
 import ru.spbau.bachelors2015.veselov.multiset.immutable
 
-// TODO: nonEmpty sets
 // TODO: intersection and union
 // TODO: for-comprehension
 // TODO: pattern-matching
@@ -12,6 +11,7 @@ import ru.spbau.bachelors2015.veselov.multiset.immutable
 // TODO: apply
 class MultiSetTest extends FlatSpec with Matchers {
   // Empty multiset
+
   immutable.MultiSet.empty shouldBe immutable.MultiSet()
 
   immutable.MultiSet.empty.size shouldBe 0
@@ -22,7 +22,12 @@ class MultiSetTest extends FlatSpec with Matchers {
 
   immutable.MultiSet.empty.add(0) shouldBe immutable.MultiSet(0)
 
-  immutable.MultiSet.empty.add(0) should not be theSameInstanceAs (immutable.MultiSet.empty)
+  immutable.MultiSet.empty.add(0, 1) shouldBe immutable.MultiSet(0, 1)
+
+  immutable.MultiSet.empty.add(0, 0) shouldBe immutable.MultiSet(0, 0)
+
+  private val emptySet = immutable.MultiSet.empty
+  emptySet.add(0) should not be theSameInstanceAs (emptySet)
 
   immutable.MultiSet.empty.count(0) shouldBe 0
 
@@ -32,5 +37,75 @@ class MultiSetTest extends FlatSpec with Matchers {
 
   immutable.MultiSet.empty[Any].flatMap((_: Any) => List(0)) shouldBe immutable.MultiSet.empty
 
-  // ...
+  // Multiset of ints
+
+  immutable.MultiSet(0) shouldBe immutable.MultiSet(0)
+
+  immutable.MultiSet(0, 1) shouldBe immutable.MultiSet(0, 1)
+
+  immutable.MultiSet(0, 1) shouldBe immutable.MultiSet(1, 0)
+
+  immutable.MultiSet(0, 0) shouldBe immutable.MultiSet(0, 0)
+
+  immutable.MultiSet(0, 0) should not be immutable.MultiSet(0)
+
+  immutable.MultiSet(0).size shouldBe 1
+
+  immutable.MultiSet(0, 1).size shouldBe 2
+
+  immutable.MultiSet(1, 0).size shouldBe 2
+
+  immutable.MultiSet(0, 0).size shouldBe 2
+
+  immutable.MultiSet(0).isEmpty shouldBe false
+
+  immutable.MultiSet(0).nonEmpty shouldBe true
+
+  immutable.MultiSet(0).add(1) shouldBe immutable.MultiSet(0, 1)
+
+  immutable.MultiSet(0).add(1, 2) shouldBe immutable.MultiSet(0, 1, 2)
+
+  immutable.MultiSet(0).add(1, 2) shouldBe immutable.MultiSet(1, 0, 2)
+
+  immutable.MultiSet(0).add(1, 2) shouldBe immutable.MultiSet(2, 0, 1)
+
+  immutable.MultiSet(0).add(0) shouldBe immutable.MultiSet(0, 0)
+
+  immutable.MultiSet(0).add(0, 1) shouldBe immutable.MultiSet(0, 0, 1)
+
+  immutable.MultiSet(0).add(0, 0) shouldBe immutable.MultiSet(0, 0, 0)
+
+  private val oneElementSet = immutable.MultiSet(0)
+  oneElementSet.add(1) should not be theSameInstanceAs (oneElementSet)
+
+  immutable.MultiSet(0).count(1) shouldBe 0
+
+  immutable.MultiSet(0).count(0) shouldBe 1
+
+  immutable.MultiSet(0, 0).count(1) shouldBe 0
+
+  immutable.MultiSet(0, 0).count(0) shouldBe 2
+
+  immutable.MultiSet(0, 1, 1).count(0) shouldBe 1
+
+  immutable.MultiSet(0, 1, 1).count(1) shouldBe 2
+
+  immutable.MultiSet(0, 1).filter(n => n > 0) shouldBe immutable.MultiSet(1)
+
+  immutable.MultiSet(0, 1, 1).filter(n => n > 0) shouldBe immutable.MultiSet(1, 1)
+
+  immutable.MultiSet(0, 0).filter(n => n > 0) shouldBe immutable.MultiSet.empty
+
+  immutable.MultiSet(0).map((n: Int) => n + 1) shouldBe immutable.MultiSet(1)
+
+  immutable.MultiSet(0, 1).map((n: Int) => n + 1) shouldBe immutable.MultiSet(1, 2)
+
+  immutable.MultiSet(0, 0).map((n: Int) => n + 1) shouldBe immutable.MultiSet(1, 1)
+
+  immutable.MultiSet(0).flatMap((n: Int) => List(n, n)) shouldBe immutable.MultiSet(0, 0)
+
+  immutable.MultiSet(0, 0).flatMap((n: Int) => List(n, n)) shouldBe immutable.MultiSet(0, 0, 0, 0)
+
+  immutable.MultiSet(0, 1, 0).flatMap((n: Int) => List(n, n)) shouldBe immutable.MultiSet(
+                                                                                  0, 0, 0, 0, 1, 1)
 }
